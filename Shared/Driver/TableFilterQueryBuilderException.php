866fb178b7acd2079f86c066b8e40b85d2910366
Martin Junger <martin.junger@keboola.com>
2022-11-24 18:39:58 +0100

    fix cs

--- Shared/Driver/TableFilterQueryBuilderException.php
+++ Shared/Driver/TableFilterQueryBuilderException.php
@@ -1,17 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Keboola\StorageDriver\Shared\Driver;
 
 use Keboola\CommonExceptions\ApplicationExceptionInterface;
-use Keboola\StorageDriver\Shared\Driver\Exception\Exception;
 use Keboola\StorageDriver\Contract\Driver\Exception\NonRetryableExceptionInterface;
+use Keboola\StorageDriver\Shared\Driver\Exception\Exception;
 use Throwable;
 
 final class TableFilterQueryBuilderException extends Exception implements
     ApplicationExceptionInterface,
     NonRetryableExceptionInterface
 {
-    public function __construct(string $message, Throwable $previous = null)
+    public function __construct(string $message, ?Throwable $previous = null)
     {
         parent::__construct($message, 0, $previous);
     }
